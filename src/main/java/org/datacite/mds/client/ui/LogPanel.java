bfe8f35c2864bb232b39c2a94105191f60388410
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-10-14 14:53:10 +0200

    enhance log panel

--- src/main/java/org/datacite/mds/client/ui/LogPanel.java
+++ src/main/java/org/datacite/mds/client/ui/LogPanel.java
@@ -1,8 +1,13 @@
 package org.datacite.mds.client.ui;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -10,12 +15,13 @@ import javax.swing.JTextArea;
 public class LogPanel extends JPanel {
 
     private JTextArea logArea;
+    private JCheckBox chckbxAutoScroll;
 
     /**
      * Create the panel.
      */
     public LogPanel() {
-        setLayout(new GridLayout(1, 0, 0, 0));
+        setLayout(new BorderLayout(0, 0));
 
         JScrollPane scrollPane = new JScrollPane();
         add(scrollPane);
@@ -25,11 +31,29 @@ public class LogPanel extends JPanel {
         logArea.setEditable(false);
         scrollPane.setViewportView(logArea);
 
+        JPanel panel = new JPanel();
+        add(panel, BorderLayout.SOUTH);
+        FlowLayout fl_panel = new FlowLayout(FlowLayout.LEFT, 5, 5);
+        panel.setLayout(fl_panel);
+
+        JButton btnClear = new JButton("Clear");
+        btnClear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                logArea.setText(null);
+            }
+        });
+        panel.add(btnClear);
+
+        chckbxAutoScroll = new JCheckBox("auto scroll to bottom");
+        chckbxAutoScroll.setSelected(true);
+        panel.add(chckbxAutoScroll);
+
     }
 
     public void log(Object obj) {
         logArea.append(obj + "\n");
-        logArea.setCaretPosition(logArea.getText().length());
+        if (chckbxAutoScroll.isSelected())
+            logArea.setCaretPosition(logArea.getText().length());
     }
 
 }
